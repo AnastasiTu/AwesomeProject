4c178dfa4b51c6fd47a096ba835c5902e85e623a
AnastasiTu <68783164+AnastasiTu@users.noreply.github.com>
2022-06-09 06:58:05 +0300

    Создала приложение

---
+++ src/MainLayout.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  Text
+} from 'react-native';
+import Animated from 'react-native-reanimated';
+
+export default function MainLayout({drawerAnimationStyle}) {
+    return (
+        <Animated.View
+            style={{
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center',
+								backgroundColor: 'white',
+								...drawerAnimationStyle
+            }}
+        >
+            <Text>MainLayout</Text>
+        </Animated.View>
+    )
+}
